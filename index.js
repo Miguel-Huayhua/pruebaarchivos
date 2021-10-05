3272cb0545dff684eb7833daa91d859cb430f9b4
Miguel Huayhua <miguelhuayhuac@gmail.com>
2021-10-04 22:37:31 -0400

    'ok'

--- index.js
+++ index.js
@@ -12,23 +12,10 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 app.use(fileupload({ createParentPath: true }))
-app.set("puerto", process.env.PORT || 3000)
+app.set("puerto", process.env.PORT)
 
-const ini = (req, res, next) => {
-    let PDF = new pdf
-    PDF.pipe(fs.createWriteStream("./file/hola.pdf", "utf-8"))
-    PDF.end()
-    next();
-}
 
-app.use(ini)
-app.get("/prueba", (req, res) => {
-    let data = {
-        nombre: "Miguel",
-        apellido: "Huayhua Condori"
-    }
-    res.json(data)
-})
+
 
 app.post('/file', (req, res, next) => {
     let { myfile } = req.files
@@ -38,7 +25,7 @@ app.post('/file', (req, res, next) => {
 }, (req, res, next) => {
     let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
     let PDF = new pdf()
-    PDF.pipe(fs.createWriteStream("./file/hola.pdf", "utf-8"))
+    PDF.pipe(fs.createWriteStream("./file/carta.pdf", "utf-8"))
     PDF.rect(10, 10, PDF.page.width - 20, PDF.page.height - 20).fillAndStroke('#fff', '#000');
     PDF.image('./friends.png', 30, 30, { width: 50, height: 50 })
     let date = new Date()
@@ -69,7 +56,6 @@ app.post('/file', (req, res, next) => {
     PDF.text('Tú', 445, 685)
     PDF.end();
     
-
     next()
 }, (req, res) => {
     res.json({ done: true })
@@ -77,30 +63,17 @@ app.post('/file', (req, res, next) => {
         if(err) throw err
     })
 })
-app.get('/file', (req, res) => {
-    res.download('hola.pdf');
-})
 
-app.post('/form', (req, res) => {
-    console.log(req.body)
-    res.send("jejej ok")
-})
+
 
 app.get('/download', (req, res) => {
-    res.download('./file/hola.pdf')
+    res.download('./file/carta.pdf')
 })
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(
     app.get("puerto"),
     () =>
         console.log(`Listen to ${app.get("puerto")}`)
 )
-
-app.get('/prueba2', (req, res) => {
-
-    res.end()
-})
-app.get('/file', (req, res) => {
-    res.download('./file/hola.pdf');
-
-})
